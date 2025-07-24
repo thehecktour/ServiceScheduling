080093f3395e526964eea80ab8a4339504d1e06a
Atilio Hector <thehacktour@gmail.com>
2025-07-24 08:13:26 -0300

    feat: create Service model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.PositiveIntegerField(help_text="Duração em minutos")
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
