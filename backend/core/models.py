a40d7c3fe6819efc8a6ee4864a6041b48fd8272d
Atilio Hector <thehacktour@gmail.com>
2025-07-24 08:14:28 -0300

    feat: create Appointment model

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,3 +8,18 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pendente"),
+        ("confirmed", "Confirmado"),
+        ("cancelled", "Cancelado"),
+    ]
+    user = models.ForeignKey(backend.src.settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+
+    def __str__(self):
+        return f"{self.user} - {self.service} em {self.date_time}"
