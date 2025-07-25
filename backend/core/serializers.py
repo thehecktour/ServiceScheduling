4debfe7deea3552894e10c14c16c68f5dad51dc2
Atilio Hector <thehacktour@gmail.com>
2025-07-25 13:51:13 -0300

    feat: add serializer for user registration

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Service, Appointment
+from django.contrib.auth.models import User
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +13,18 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         fields = '__all__'
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
