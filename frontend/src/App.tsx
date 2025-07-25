bee64165c9e9ccff883729a3d25bbe286995943e
Atilio Hector <thehacktour@gmail.com>
2025-07-25 14:13:21 -0300

    feat: add app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-function App() {
+import Auth from "./pages/Auth";
+import ServiceCatalog from "./pages/ServiceCatalog";
+import AppointmentForm from "./pages/AppointmentForm";
+import History from "./pages/History";
+import AdminDashboard from "./pages/AdminDashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
+export default function App() {
   return (
-    <>
-
-    </>
-  )
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<ServiceCatalog />} />
+            <Route path="/agendar" element={<AppointmentForm />} />
+            <Route path="/historico" element={<History />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
-
-export default App
