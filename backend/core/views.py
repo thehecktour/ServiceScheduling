99ccdb629d0692b698b185536a7c2cc407c68ca2
Atilio Hector <thehacktour@gmail.com>
2025-07-25 11:54:27 -0300

    feat: add views setup inside core app

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Service, Appointment
+from .serializers import ServiceSerializer, AppointmentSerializer
+from .permissions import IsOwnerOrAdmin
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Appointment.objects.all()
+        return Appointment.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
