b6cacfabc71c067172043ca72ae44eef71ac17af
Atilio Hector <thehacktour@gmail.com>
2025-07-25 14:03:31 -0300

    feat: add store file using reducer

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import servicesReducer from "../features/services/servicesSlice";
+import appointmentsReducer from "../features/appointments/appointmentsSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    services: servicesReducer,
+    appointments: appointmentsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
