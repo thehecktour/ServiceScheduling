555786cb2e27dac5452924af258adf58fa6f96c0
Atilio Hector <thehacktour@gmail.com>
2025-07-25 13:52:36 -0300

    feat: add url for user registration

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet, AppointmentViewSet
+from .views import ServiceViewSet, AppointmentViewSet, UserRegistrationView
 
 router = DefaultRouter()
 router.register(r'services', ServiceViewSet)
-router.register(r'appointments', AppointmentViewSet)
+router.register(r'appointments', AppointmentViewSet, basename='appointment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
 ]
